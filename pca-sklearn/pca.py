5e9fe943e29d95a1bba8011d4885080dfe3d6b53
chiennghe <chienptit@gmail.com>
2019-10-11 22:26:08 +0700

    pca with scikit learn

---
+++ pca-sklearn/pca.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('pca-sklearn/Wine.csv')
+X = dataset.iloc[:, 0:13].values
+y = dataset.iloc[:, 13].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Applying PCA
+from sklearn.decomposition import PCA
+pca = PCA(n_components = None)
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+explained_variance = pca.explained_variance_ratio_
